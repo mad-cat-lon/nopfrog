3e0579277ffb5d64072939acb748156d9bc543be
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-11-27 22:06:29 -0500

    Removed unneeded check

--- src/utils/pid2pname.c
+++ src/utils/pid2pname.c
@@ -1,7 +1,4 @@
 int pid_to_pname(char *pid, char *buf) {
-    if (strspn(pid, "0123456789") != strlen(pid)) {
-        return 0;
-    }
     char tmp[4096];
     snprintf(tmp, sizeof(tmp), "/proc/%s/stat", pid);
     FILE* f = fopen(tmp, "r");
@@ -14,12 +11,5 @@ int pid_to_pname(char *pid, char *buf) {
     int u;
     sscanf(tmp, "%d (%[^)]s", &u, buf);
     return 1;
-    // char cmd[8192];
-    // sprintf(cmd, "cat /proc/%s/cmdline", pid);
-    // FILE *f = popen(cmd, "r");
-    // if (fread(buf, 4096, 1, fp) != 0) {
-    //     return 0;
-    // }
-    // return 1;
     
 }
